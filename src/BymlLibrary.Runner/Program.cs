d297cf724760febef72810d74f3c267ab0f4865b
ArchLeaders <archleadership28@gmail.com>
2024-01-11 02:20:13 -0800

    Update runner

--- src/BymlLibrary.Runner/Program.cs
+++ src/BymlLibrary.Runner/Program.cs
@@ -1,11 +1,13 @@
-﻿using BenchmarkDotNet.Running;
-using BymlLibrary;
+﻿#if RELEASE
+using BenchmarkDotNet.Running;
 using BymlLibrary.Runner.Benchmarks;
-using BymlLibrary.Yaml;
-using Revrs;
 
 BenchmarkRunner.Run<BymlBenchmarks>();
 return;
+#else
+using BymlLibrary;
+using BymlLibrary.Yaml;
+using Revrs;
 
 byte[] buffer = File.ReadAllBytes(args[0]);
 RevrsReader reader = new(buffer);
@@ -16,7 +18,11 @@ emitter.Emit(immutableByml);
 
 File.WriteAllText("D:\\bin\\Byml-v7\\test.yml", emitter.Builder.ToString());
 
+using FileStream fs = File.Create(args[1]);
 Byml byml = Byml.FromBinary(buffer);
+byml.WriteBinary(fs, byml.Endianness);
+
+// Console.WriteLine(byml.GetMap()["PtclBin"].GetBinaryAligned().Alignment);
 // Console.WriteLine(
 //     byml
 //         .Get<IDictionary<string, Byml>>()["Actors"]
@@ -24,3 +30,4 @@ Byml byml = Byml.FromBinary(buffer);
 //         .Get<IDictionary<string, Byml>>()["name"]
 //         .Get<string>()
 // );
+#endif
