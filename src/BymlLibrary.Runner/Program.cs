beedc0780e1e6fcdbe7170f21aebb6d7a0e108dc
ArchLeaders <archleadership28@gmail.com>
2024-01-12 17:58:31 -0800

    Misc runner updates

--- src/BymlLibrary.Runner/Program.cs
+++ src/BymlLibrary.Runner/Program.cs
@@ -9,7 +9,7 @@ return;
 using BymlLibrary;
 using Revrs;
 
-byte[] buffer = File.ReadAllBytes(args[1]);
+byte[] buffer = File.ReadAllBytes(args[0]);
 RevrsReader reader = new(buffer);
 ImmutableByml immutableByml = new(ref reader);
 
@@ -17,17 +17,6 @@ string yaml = immutableByml.ToYaml();
 File.WriteAllText("D:\\bin\\Byml-v7\\test.yml", yaml);
 
 Byml byml = Byml.FromText(yaml);
-Console.WriteLine(byml.GetMap()["Actors"].GetArray()[0].GetMap()["name"].GetString());
+byml.WriteBinary(args[1], immutableByml.Endianness);
 
-// Byml byml = Byml.FromBinary(buffer);
-// byml.WriteBinary(args[1], Endianness.Big);
-
-// Console.WriteLine(byml.GetMap()["PtclBin"].GetBinaryAligned().Alignment);
-// Console.WriteLine(
-//     byml
-//         .Get<IDictionary<string, Byml>>()["Actors"]
-//         .Get<IList<Byml>>()[3]
-//         .Get<IDictionary<string, Byml>>()["name"]
-//         .Get<string>()
-// );
 #endif
